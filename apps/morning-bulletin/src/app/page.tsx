e3bd97ffc7f46ac07d95b545ed50b1969743c5f0
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2024-09-09 17:24:05 +0800

    add new components to homepage

--- apps/morning-bulletin/src/app/page.tsx
+++ apps/morning-bulletin/src/app/page.tsx
@@ -1,14 +1,17 @@
 'use client'
 
 import { Provider } from 'react-redux'
+import { FeaturedArticle } from './components/FeaturedArticle'
 import Layout from './components/layout'
 import { store } from './utils/store'
+import { LatestNews } from './components/LatestNews'
 
 export default function Index() {
     return (
         <Provider store={store}>
             <Layout>
-                <>Hello world</>
+                <FeaturedArticle articleId={'1'} />
+                <LatestNews />
             </Layout>
         </Provider>
     )
