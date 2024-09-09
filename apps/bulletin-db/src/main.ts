16c1356c74d66d8b24279d650e810a3ab8122e63
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2024-09-09 14:47:55 +0800

    add cors

--- apps/bulletin-db/src/main.ts
+++ apps/bulletin-db/src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     const globalPrefix = 'api'
     app.setGlobalPrefix(globalPrefix)
+    app.enableCors({ origin: [`http://localhost:3000`], credentials: true })
     const port = process.env.PORT || 3001
     await app.listen(port)
     Logger.log(
