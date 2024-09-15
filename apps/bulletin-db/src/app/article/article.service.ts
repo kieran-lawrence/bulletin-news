bd50894242a4071fa4d9a8aca41d04faf421ba85
Kieran Lawrence <kieran.lawrence@wanews.com.au>
2024-09-15 13:15:30 +0800

    include comment author in query

--- apps/bulletin-db/src/app/article/article.service.ts
+++ apps/bulletin-db/src/app/article/article.service.ts
@@ -47,6 +47,7 @@ export class ArticleService implements IArticleService {
                     status: 'live',
                 },
             )
+            .leftJoinAndSelect('comments.user', 'user')
             .getOne()
     }
     findByCategory({
